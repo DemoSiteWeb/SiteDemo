2513910c02f2366183d01c1ad52e00523f0b6be6
emvogonganoma <Eric-Patrick.MVOGONGANOMA@amadeus.com>
2020-05-03 21:54:28 +0200

    new site web

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import {Subscription} from 'rxjs';
+import {AppareilService} from './services/appareil.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'mon-projet-angular';
+export class AppComponent implements OnInit, OnDestroy {
+
+  isAuth = false;
+
+  appareils: any[];
+  secondes: number;
+  title = 'this my first view';
+  counterSubscription: Subscription;
+
+  constructor(private appareilService: AppareilService) {
+    setTimeout(
+      () => {
+        this.isAuth = true;
+      }, 4000
+    );
+  }
+  // ngOnInit() {
+  //   this.appareils = this.appareilService.appareils;
+  // }
+  ngOnInit() {
+    this.appareils = this.appareilService.appareils;
+    const counter = Observable.interval(1000);
+    this.counterSubscription = counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      (error) => {
+        console.log('Uh-oh, an error occurred! : ' + error);
+      },
+      () => {
+        console.log('Observable complete!');
+      }
+    );
+  }
+
+  ngOnDestroy() {
+    this.counterSubscription.unsubscribe();
+  }
 }
